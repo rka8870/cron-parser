2b39f1fa404a5258424f7ba89e96006fa225cead
rka8870 <rka8870@gmail.com>
2024-09-01 20:41:44 +0530

    Added another test case

--- src/test/java/CronParserTest.java
+++ src/test/java/CronParserTest.java
@@ -11,12 +11,25 @@ public class CronParserTest {
     private CronParser cronParser = new CronParser();;
 
     @Test
-    public void testValidCronExpression() throws Exception {
+    public void testValidCronExpression1() throws Exception {
         String expression = "*/15 0 1,15 * 1-5 /usr/bin/find";
         List<String> result = cronParser.parseExpression(expression);
         assertEquals(6, result.size());
         assertEquals("minute        0 15 30 45", result.get(0));
         assertEquals("hour          0", result.get(1));
+        assertEquals("day of month  1 15", result.get(2));
+        assertEquals("month         1 2 3 4 5 6 7 8 9 10 11 12", result.get(3));
+        assertEquals("day of week   1 2 3 4 5", result.get(4));
+        assertEquals("command       /usr/bin/find", result.get(5));
+    }
+
+    @Test
+    public void testValidCronExpression2() throws Exception {
+        String expression = "*/15 2,4-6,9/3 1,15 * 1-5 /usr/bin/find";
+        List<String> result = cronParser.parseExpression(expression);
+        assertEquals(6, result.size());
+        assertEquals("minute        0 15 30 45", result.get(0));
+        assertEquals("hour          2 4 5 6 9 12 15 18 21", result.get(1));
         assertEquals("day of month  1 15", result.get(2));
         assertEquals("month         1 2 3 4 5 6 7 8 9 10 11 12", result.get(3));
         assertEquals("day of week   1 2 3 4 5", result.get(4));
